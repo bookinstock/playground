a98064708d7eed4a91898ff68b008c175861f3b9
Wende Lu <wendelu1991@gmail.com>
2022-05-01 23:06:00 +0800

    number

--- go/testapp/data_type/data_type.go
+++ go/testapp/data_type/data_type.go
@@ -1,7 +1,22 @@
 package data_type
 
+import "fmt"
+
+const (
+	const_a = 1
+	const_b = 2
+)
+
+var (
+	var_a = 1
+	var_b = 2
+)
+
 func Run() {
 	playBool()
 	playNumber()
 	playString()
+
+	fmt.Println("const=", const_a, const_b)
+	fmt.Println("var=", var_a, var_b)
 }
--- go/testapp/data_type/number.go
+++ go/testapp/data_type/number.go
@@ -6,4 +6,9 @@ func playNumber() {
 	fmt.Println("1 / 2=", 1/2)
 	fmt.Println("1 / 2.0=", 1/2.0)
 	fmt.Println("1 % 2=", 1%2)
+
+	fmt.Println("1 million=", 1_000_000)
+	fmt.Println("1 million=", 1e6)
+
+	fmt.Println("int64 =", int64(100))
 }
