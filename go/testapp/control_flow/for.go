af6b1e179dc6c00153d728b0fa938d2dd523ca08
Wende Lu <wendelu1991@gmail.com>
2022-05-02 18:10:57 +0800

    for

--- go/testapp/control_flow/for.go
+++ go/testapp/control_flow/for.go
@@ -15,4 +15,23 @@ func play_for() {
 	}
 	fmt.Println()
 
+	i = 0
+	for {
+		if i == 2 {
+			i += 1
+			continue
+		}
+		if i == 5 {
+			break
+		}
+		fmt.Print(i, " ")
+		i += 1
+	}
+	fmt.Println()
+
+	lst := []int{1, 2, 3, 4, 5}
+	for i, e := range lst {
+		fmt.Println("i=", i, "e=", e)
+	}
+	fmt.Println()
 }
