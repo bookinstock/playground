741b83df9b7c14dbd5a0105dfc4799de0467104f
Wende Lu <wendelu1991@gmail.com>
2022-05-09 11:43:52 +0800

    embed

--- go/testapp/object/embed.go
+++ go/testapp/object/embed.go
@@ -1,17 +1,63 @@
 package object
 
-func play_embed() {
+import "fmt"
 
+func play_embed() {
+	fmt.Println("===embed===")
+	a := A{Base: Base{V: 1, II: F1{f: 2}}, VA: 3}
+	b := B{Base: Base{V: 1, II: F2{f: 4}}, VB: 5}
+	fmt.Printf("a=%#v\n", a)
+	fmt.Printf("b=%#v\n", b)
+	fmt.Println("a v=", a.v())
+	fmt.Println("b v=", b.v())
+}
+
+type Base struct {
+	V int
+
+	II
+}
+
+func (base Base) value() int {
+	return base.V
 }
 
 type A struct {
-	v int
+	Base
+
+	VA int
 }
 
-func (a A) value() int {
-	return a.v
+type B struct {
+	Base
+
+	VB int
 }
 
-type AA struct {
-	A
+func (a *A) v() int {
+	return a.value() + a.VA + a.foo()
+}
+
+func (b *B) v() int {
+	return b.value() + b.VB + b.foo()
+}
+
+type II interface {
+	foo() int
+}
+
+type F1 struct {
+	f int
+}
+
+type F2 struct {
+	f int
+}
+
+func (f F1) foo() int {
+	return f.f
+}
+
+func (f F2) foo() int {
+	return f.f
 }
