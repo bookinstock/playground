c9df7ab737bd51ce3fe21548b21261b742ec4d31
Wende Lu <wendelu1991@gmail.com>
2022-05-22 07:28:28 +0800

    clean

--- go/testapp/data_type/data_type.go
+++ go/testapp/data_type/data_type.go
@@ -1,30 +1,16 @@
 package data_type
 
-import "fmt"
-
-const (
-	const_a = 1
-	const_b = 2
-)
-
-var (
-	var_a = 1
-	var_b = 2
-)
-
 func Run() {
-	checkVarConst()
 
 	playBool()
+
 	playNumber()
+
 	playString()
+
 	play_slice()
+
 	play_map()
 
 	play_pointer()
-}
-
-func checkVarConst() {
-	fmt.Println("const=", const_a, const_b)
-	fmt.Println("var=", var_a, var_b)
 }
